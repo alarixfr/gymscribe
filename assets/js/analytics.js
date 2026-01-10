6c0f5eec77d525593ca7f0c4c47cfd5cf8386f9d
Alaric Abyasa <alaricabyasa11@gmail.com>
2026-01-11 03:53:05 +0700

    members handler fixes

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,3 +1,34 @@
 import { init } from './handler.js';
 
-init();
+const allCount = document.getElementById('allCount');
+const activeCount = document.getElementById('activeCount');
+const expiresSoonCount = document.getElementById('expiresSoonCount');
+const expiredCount = document.getElementById('expiredCount');
+const attendedCount = document.getElementById('attendedCount');
+const absenceCount = document.getElementById('absenceCount');
+
+let member;
+
+async function loadMembers() {
+  try {
+    await init();
+    allCount.textContent = 'Fetching...';
+    activeCount.textContent = 'Fetching...';
+    expiresSoonCount.textContent = 'Fetching...';
+    expiredCount.textContent = 'Fetching...';
+    
+    await init();
+    members = await getMembers();
+    
+    if (members?.error) throw new Error(members.error);
+    
+    allCount.textContent = members.membersCount.all;
+    activeCount.textContent = members.membersCount.active;
+    expiresSoonCount.textContent = members.membersCount.expiresSoon;
+    expiredCount.textContent = members.membersCount.expired;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+loadMembers();
--- assets/js/members.js
+++ assets/js/members.js
@@ -1,40 +1,12 @@
 import { init, getMembers } from './handler.js';
 import { createModal, deleteModal } from './modal.js';
 
-const allCount = document.getElementById('allCount');
-const activeCount = document.getElementById('activeCount');
-const expiresSoonCount = document.getElementById('expiresSoonCount');
-const expiredCount = document.getElementById('expiredCount');
-const attendedCount = document.getElementById('attendedCount');
-const absenceCount = document.getElementById('absenceCount');
-
 const viewButtons = document.querySelectorAll('.view-btn');
 const editButtons = document.querySelectorAll('.edit-btn');
 const renewButtons = document.querySelectorAll('.renew-btn');
 const removeButtons = document.querySelectorAll('.remove-btn');
 
 let members;
-
-async function loadMembers() {
-  try {
-    allCount.textContent = 'Fetching...';
-    activeCount.textContent = 'Fetching...';
-    expiresSoonCount.textContent = 'Fetching...';
-    expiredCount.textContent = 'Fetching...';
-    
-    await init();
-    members = await getMembers();
-    
-    if (members?.error) throw new Error(members.error);
-    
-    allCount.textContent = members.membersCount.all;
-    activeCount.textContent = members.membersCount.active;
-    expiresSoonCount.textContent = members.membersCount.expiresSoon;
-    expiredCount.textContent = members.membersCount.expired;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
 
 viewButtons.forEach(btn => {
   btn.addEventListener('click', (e) => {
@@ -59,7 +31,3 @@ removeButtons.forEach(btn => {
     createModal('memberRemove');
   });
 });
-
-(async () => {
-  await loadMembers;
-})();
