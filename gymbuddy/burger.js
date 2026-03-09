2fd3890be9bb9cc4a16110f00c836e56c71908eb
Alaric Abyasa <alaricabyasa11@gmail.com>
2026-03-09 17:52:41 +0700

    GymBuddy page responsiveness

---
+++ gymbuddy/burger.js
@@ -0,0 +1,21 @@
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+
+let opened = false;
+
+burger.addEventListener('click', () => {
+  if (opened === true) return;
+  opened = true;
+  burger.classList.add('open');
+  if (navbar.style.display === 'none') {
+    navbar.style.display = 'flex';
+  } else {
+    navbar.style.display = 'none';
+  }
+  setTimeout(() => {
+    burger.classList.remove('open');
+    opened = false;
+  }, 200);
+});
+
+navbar.style.display = 'none';
